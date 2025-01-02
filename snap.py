6292ba610c3085f36ef24bf24f45b8fb3159f97f
Cecilia Martin <cecilia.gmartin@gmail.com>
2025-01-02 04:41:40 -0800

    added authentication with token

--- snap.py
+++ snap.py
@@ -26,6 +26,7 @@
 from dataclasses import dataclass, field
 from typing import TypedDict, List, Dict, Set, Optional
 from qumulo.rest_client import RestClient
+from qumulo.lib.auth import Credentials
 import argparse
 import csv
 import getpass
@@ -71,15 +72,34 @@ class SnapPolicyInfo:
 class Client:
     def __init__(self, creds: Creds):
         self.creds = creds
-        self.rc = RestClient(self.creds["QHOST"], self.creds["QPORT"])
+        self.rc = None
         self.login()
 
-    def login(self) -> None:
+    def login(self):
+        if "QTOKEN" in self.creds:
+            self.token_login()
+        else:
+            self.user_login()
+
+    def token_login(self) -> None:
         try:
-            self.rc.login(self.creds["QUSER"], self.creds["QPASS"])
-            logger.info("Successfully logged in.")
+            self.rc = RestClient(
+                address=self.creds["QHOST"],
+                port=self.creds["QPORT"],
+                credentials=Credentials(self.creds["QTOKEN"]),
+            )
+            logger.info("Successfully logged in with authentication token.")
         except Exception as e:
-            logger.error(f"Login failed: {e}")
+            logger.error(f"Token login failed: {e}")
+            sys.exit(1)
+
+    def user_login(self) -> None:
+        try:
+            self.rc = RestClient(address=self.creds["QHOST"], port=self.creds["QPORT"])
+            self.rc.login(self.creds["QUSER"], self.creds["QPASS"])
+            logger.info("Successfully logged in with username and password.")
+        except Exception as e:
+            logger.error(f"User login failed: {e}")
             sys.exit(1)
 
 
@@ -311,9 +331,14 @@ class Snapshot:
 def main() -> None:
     # Set up argument parsing
     parser = argparse.ArgumentParser(description="Qumulo cluster credentials")
-    parser.add_argument("--ip", type=str, help="Qumulo cluster IP address")
+    parser.add_argument(
+        "--host", type=str, required=True, help="Qumulo node IP address or FQDN"
+    )
     parser.add_argument("--username", type=str, help="Username for the Qumulo cluster")
     parser.add_argument("--password", type=str, help="Password for the Qumulo cluster")
+    parser.add_argument(
+        "--token", type=str, help="Authentication token for the Qumulo cluster"
+    )
     parser.add_argument(
         "--action",
         type=str,
@@ -325,19 +350,22 @@ def main() -> None:
     )
     args = parser.parse_args()
 
-    # If no arguments are provided, prompt interactively
-    ip = args.ip if args.ip else input("Enter the Qumulo cluster IP address: ")
-    username = args.username if args.username else input("Enter your username: ")
-    password = (
-        args.password if args.password else getpass.getpass("Enter your password: ")
-    )
+    if not args.token and not args.username:
+        parser.error("Either --username or --token must be provided.")
 
     creds: Creds = {
-        "QHOST": ip,
-        "QUSER": username,
-        "QPASS": password,
+        "QHOST": args.host,
         "QPORT": 8000,  # Default Qumulo REST API port
     }
+
+    if args.token:
+        creds["QTOKEN"] = args.token
+    else:
+        password = (
+            args.password if args.password else getpass.getpass("Enter your password: ")
+        )
+        creds["QUSER"] = args.username
+        creds["QPASS"] = password
 
     client = Client(creds)
     snapshot = Snapshot(client)
